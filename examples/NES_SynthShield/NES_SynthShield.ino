c8944c3e8d3e2c7f950cb33e038fd3d58f9ef88e
shiftmore <nate.kopp@gmail.com>
2013-12-13 01:04:48 -0600

    Update NES_SynthShield.ino
    Changed switch statements to ifs to support multiple wave gens controlled by the same midi channel.
    Set all wave gen channels to 1 so they play in unison by default. 
    Moved CC parameter assignments to constants.

--- examples/NES_SynthShield/NES_SynthShield.ino
+++ examples/NES_SynthShield/NES_SynthShield.ino
@@ -2,9 +2,19 @@
 #include <MIDI.h>
 #include <SPI.h>
 
+// The MIDI channel that will control each wave gen.
+// Default: all set to channel 1, all wave gens play in unison
 #define CHANNEL_REC1 1
-#define CHANNEL_REC2 2
-#define CHANNEL_TRI  3
+#define CHANNEL_REC2 1
+#define CHANNEL_TRI  1
+
+// Here is where you can configure which knob controls which parameter. 
+#define CC_RECT1_DUTYCYCLE  0x4B //75
+#define CC_RECT2_DUTYCYCLE  0x4C //76
+#define CC_RECT1_VOLUME     0x5C //92
+#define CC_RECT2_VOLUME     0x5F //95
+#define CC_RECT1_ATTACK     0x0C //12
+#define CC_RECT2_ATTACK     0x0D //13
 
 #define MIDI_NOTE_OFFSET 21
 
@@ -12,96 +22,56 @@ NES2A03 nes;
 
 void handleNoteOn(byte c,byte n,byte v){
   if(n < MIDI_NOTE_OFFSET) return;
-  switch(c){
-    case CHANNEL_REC1:
-        nes.rectangle1.handleNoteOn(c,n-MIDI_NOTE_OFFSET,v);
-        break;
-    case CHANNEL_REC2:
-        nes.rectangle2.handleNoteOn(c,n-MIDI_NOTE_OFFSET,v);
-        break;
-    case CHANNEL_TRI:
-        nes.triangle.handleNoteOn(c,n-MIDI_NOTE_OFFSET,v);
-        break;
+
+  if(v == 0){
+    handleNoteOff(c,n,v); //direct midi keyboard sends 0 velocity rather than note off command
+    return;
   } 
+
+  //Use a series of ifs rather than a switch to support controlling multiple wave gens with the same midi channel
+  if(c == CHANNEL_REC1) nes.rectangle1.handleNoteOn(c,n-MIDI_NOTE_OFFSET,v);
+  if(c == CHANNEL_REC2) nes.rectangle2.handleNoteOn(c,n-MIDI_NOTE_OFFSET,v);
+  if(c == CHANNEL_TRI) nes.triangle.handleNoteOn(c,n-MIDI_NOTE_OFFSET,v);
+
 }
 
 void handleNoteOff(byte c,byte n,byte v){
   if(n < MIDI_NOTE_OFFSET) return;
-  switch(c){
-    case CHANNEL_REC1:
-        nes.rectangle1.handleNoteOff(c,n-MIDI_NOTE_OFFSET,v);
-        break;
-    case CHANNEL_REC2:
-        nes.rectangle2.handleNoteOff(c,n-MIDI_NOTE_OFFSET,v);
-        break;
-    case CHANNEL_TRI:
-        nes.triangle.handleNoteOff(c,n-MIDI_NOTE_OFFSET,v);
-        break;
-  }  
+
+  if(c == CHANNEL_REC1) nes.rectangle1.handleNoteOff(c,n-MIDI_NOTE_OFFSET,v);
+  if(c == CHANNEL_REC2) nes.rectangle2.handleNoteOff(c,n-MIDI_NOTE_OFFSET,v);
+  if(c == CHANNEL_TRI) nes.triangle.handleNoteOff(c,n-MIDI_NOTE_OFFSET,v);
+
 }
 
 void handlePitchBend(byte c,int b){
-  switch(c){
-    case CHANNEL_REC1:
-        nes.rectangle1.handlePitchBend(c,b);
-        break;
-    case CHANNEL_REC2:
-        nes.rectangle2.handlePitchBend(c,b);
-        break;
-    case CHANNEL_TRI:
-        nes.triangle.handlePitchBend(c,b);
-        break;
-  }  
+
+ if(c == CHANNEL_REC1) nes.rectangle1.handlePitchBend(c,b);
+ if(c == CHANNEL_REC2) nes.rectangle2.handlePitchBend(c,b);
+ if(c == CHANNEL_TRI) nes.triangle.handlePitchBend(c,b);
+  
 }
 
 void handleCC(byte channel, byte number, byte value){
   switch(number){
-  	case 0x4B:
+  	case CC_RECT1_DUTYCYCLE:
       nes.rectangle1._setDutyCycle(value);
       break;  
-    case 0x4C:
+    case CC_RECT2_DUTYCYCLE:
       nes.rectangle2._setDutyCycle(value);
       break;  
-    case 0x5C:
+    case CC_RECT1_VOLUME:
       nes.rectangle1._setVolume(value);
       break;  
-    case 0x5F:
+    case CC_RECT2_VOLUME:
       nes.rectangle2._setVolume(value);
       break;  
-    case 0x0C:
+    case CC_RECT1_ATTACK:
       nes.rectangle1._setAttack(value);
       break;  
-    case 0x0D:
+    case CC_RECT2_ATTACK:
       nes.rectangle2._setAttack(value);
       break;  
-  	/*
-    case 0x0C:
-      setArpRate(value);
-      break;
-    case 0x5B:
-      setDutyCycle(0,value);
-      break;  
-    case 0x5D:
-      setDutyCycle(1,value);
-      break;  
-    case 0x5F:
-      setRect2Detune(value);
-      break;
-    case 0x49:
-      setAttackValue(value);
-      break;
-    case 0x48:
-      setReleaseValue(value);
-      break;  
-    case 0x0B:
-      setVelocitySensitivityValue(value);
-      break;
-    case 0x05:
-      setRect1Volume(value); 
-      break;   
-    case 0x54:
-      setRect2Volume(value);
-     */
     default:
      break; 
   }
@@ -123,6 +93,11 @@ void MIDISetup(){
 void setup() {  
 	nes.init(); 
 	MIDISetup();  
+
+  //set some default values so we can hear something..
+  nes.rectangle1._setVolume(127);
+  nes.rectangle2._setVolume(127);
+  nes.triangle._setVolume(127);
 }
 void loop() {
 	MIDI.read();									// Call MIDI.read the fastest you can for real-time performance.
